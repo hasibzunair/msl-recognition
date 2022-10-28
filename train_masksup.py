e29aa53e9b789534697e6d6fdf04c0be3795ce67
hasibzunair <hasibzunair@gmail.com>
2022-10-28 12:08:57 -0400

    run masksup coco

--- train_masksup.py
+++ train_masksup.py
@@ -112,6 +112,10 @@ def train_masksup(i, args, model, train_loader, optimizer, warmup_scheduler):
         # 0.4,0.4,0.2 -> 94.5 mAP
         # 0.3,0.2,0.5 -> 95.2 mAP!!!
 
+        # COCO
+        # 0.3,0.2,0.5 -> 85.33 mAP
+        # base ??
+        
         # todoo
         # ...
 
