44455f6c508d8cc5bd03ad910dc0d3ecfeb08f07
hasibzunair <hasibzunair@gmail.com>
2023-01-13 17:40:15 -0500

    go back to initial masksup, reproduce segmentation results

--- semantic_segmentation/run_segmentation_scripts.sh
+++ semantic_segmentation/run_segmentation_scripts.sh
@@ -1,3 +1,9 @@
 python trainval_glas_polyp.py --exp_name glas_bin_levit
 python trainval_glas_polyp.py --exp_name polyp_bin_levit
 python trainval_nyudv2.py --exp_name nyu_bin_nestunet
+
+# https://arxiv.org/abs/2210.00923
+# Method GLaS, mIoU (↑) CVC-Clinic-DB, mIoU (↑) NYUDv2 (↑)
+# MAE [8] 75.04 82.50 37.42
+# MaskSup (BMVC) 76.06 84.02 39.31
+# 
