97f744b9dac97a12acd4e95027c0df6e789211b4
hasibzunair <hasibzunair@gmail.com>
2023-10-27 12:52:26 -0400

    remove all files from branch

--- semantic_segmentation/run_segmentation_scripts.sh
+++
@@ -1,8 +0,0 @@
-python trainval_glas_polyp.py --exp_name glas_levit_bin
-python trainval_glas_polyp.py --exp_name polyp_levit_bin
-python trainval_nyudv2.py --exp_name nyu_nestunet_bin
-
-# https://arxiv.org/abs/2210.00923
-# Method GLaS, mIoU (↑) CVC-Clinic-DB, mIoU (↑) NYUDv2 (↑)
-# MAE [8] 75.04 82.50 37.42
-# MaskSup (BMVC) 76.06 84.02 39.31
